d08e0898ab8bb8d045bcbd6250022e1ce5933744
TatyanaBtt <2269821@gmail.com>
2024-11-07 23:23:07 +0300

    add

--- module_8_1.py
+++ module_8_1.py
@@ -2,9 +2,9 @@ def add_everything_up(a:(int or float or str), b:(int or float or str)):
     try:
         a + b
     except TypeError:
-        return(f'{a}{b}')
+        return f'{a}{b}'
     else:
-        return(f'{a+b:0.3f}')
+        return f'{a+b:0.3f}'
 
 
 print(add_everything_up(123.456, 'строка'))
